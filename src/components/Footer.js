3e26d92cfe823bb863fb2a25fc91daabb657f876
Moekyawunng <moekyawaung@diplomats.com>
2026-04-28 00:16:49 +0630

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2026 Moe Kyaw Aung. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
