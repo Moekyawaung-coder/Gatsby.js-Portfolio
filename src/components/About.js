bea49692bf34d9a197c417a20aa8163c76ed23d6
Moekyawunng <moekyawaung@diplomats.com>
2026-04-28 00:13:59 +0630

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        I am an Android Developer and Flutter Web enthusiast based in Myanmar. I build secure, modern, responsive, and highly interactive apps with clean architecture.
+      </p>
+    </section>
+  );
+};
+
+export default About;
