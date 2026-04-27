98bb921de486ce1baa8f78eb84624a08a3d074e8
Moekyawunng <moekyawaung@diplomats.com>
2026-04-28 00:10:41 +0630

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import "../styles/global.css";
+
+const IndexPage = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default IndexPage;
